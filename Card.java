b05dd17330f0beb917ce9cdba4c2d10d2a0c311e
andrew tran <31467090+wtrandrew@users.noreply.github.com>
2017-11-08 11:23:59 -0800

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,13 @@
+public class Card {
+	String name; int value;
+	public Card(String cardName, int cardValue) {
+		name =  cardName;
+		value = cardValue;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getCard() {
+		return value;
+	}
+}
