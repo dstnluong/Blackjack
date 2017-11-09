ca7d7243afec79e68224e7e863ad64c1cd087d79
Dustin Luong <dstn.luong@gmail.com>
2017-11-09 12:22:19 -0800

    added suits

--- Card.java
+++ Card.java
@@ -1,12 +1,16 @@
 public class Card {
-	String name; int value;
-	public Card(String cardName, int cardValue) {
+	String name; String suit; int value;
+	public Card(String cardName, String cardSuit, int cardValue) {
 		name =  cardName;
+        suit = cardSuit;
 		value = cardValue;
 	}
 	public String getName() {
 		return name;
 	}
+    public String getSuit(){
+        return suit;
+    }
 	public int getValue() {
 		return value;
 	}
