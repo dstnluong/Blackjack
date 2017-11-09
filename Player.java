c8a4f3ab57cd0cac11119848b8633208aea4dea5
Dustin Luong <dstn.luong@gmail.com>
2017-11-09 14:20:33 -0800

    points?

--- Player.java
+++ Player.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 public class Player {
     ArrayList <Card> hand;
+    int points;
     public Player() {
         hand = new ArrayList<Card>();
+        points = ;
+    }
+    public getPoints(){
+        return
     }
 }
