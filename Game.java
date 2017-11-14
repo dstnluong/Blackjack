200cb47bdb216d1c80a50d16d2ec5f60124a2287
andrew tran <aw3som3andr3w@gmail.com>
2017-11-14 12:35:09 -0800

    arraylist players

--- Game.java
+++ Game.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 public class Game {
 	public static void main(String[] args) {
+		int index = 0;
         Deck vegas = new Deck();
+       	ArrayList<Player> players = new ArrayList<Player>(); 
         Scanner in = new Scanner(System.in);
        	System.out.print("How many players? ");
-       	int players = in.nextInt();
-        for(int i = 1; i <= players; i++) {
-        	System.out.print("PLAYER " + i + ": ");
-        	Player a = new Player(in.next());
-        }
    	}
 }
