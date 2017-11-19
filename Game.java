dd5825e59b33390f4da1b3d04ff53ff6d19174eb
Dustin Luong <dstn.luong@gmail.com>
2017-11-18 21:26:10 -0800

    trying to to print 4 players at a time

--- Game.java
+++ Game.java
@@ -48,49 +48,51 @@ public class Game {
     }
     public void displaySidebySide(){
         String border = "+-----+";
-        for(int i = 0; i < players.size(); i++){
-            System.out.printf("%-18s", players.get(i).getUsername());
-            for(int j = 0; j < players.get(i).getHandSize() - 2; j++){
-                System.out.printf("%9s", " ");
+        int playersperline = 4;
+        for(int s = 0; s < players.size(); s = s + playersperline) {
+            for(int i = s; i < playersperline; i++){
+                System.out.printf("%-18s", players.get(i).getUsername());
+                for(int j = 0; j < players.get(i).getHandSize() - 2; j++){
+                    System.out.printf("%9s", " ");
+                }
             }
-        }
-        System.out.printf("%n");
+            System.out.printf("%n");
 
-        for(int i = 0; i < players.size(); i++){
-            for(int j = 0; j < players.get(i).getHandSize(); j++){
-                System.out.printf("%-9s", border);
+            for(int i = s; i < playersperline; i++){
+                for(int j = 0; j < players.get(i).getHandSize(); j++){
+                    System.out.printf("%-9s", border);
+                }                
             }
-        }
-        System.out.printf("%n");
+            System.out.printf("%n");
 
-        for(int i = 0; i < players.size(); i++){
-            for(int j = 0; j < players.get(i).getHandSize(); j++){
-                System.out.printf("|%-5s|  ", players.get(i).getCard(j).getRank());
+            for(int i = s; i < playersperline; i++){
+                for(int j = 0; j < players.get(i).getHandSize(); j++){
+                    System.out.printf("|%-5s|  ", players.get(i).getCard(j).getRank());
+                }
             }
-        }
-        System.out.printf("%n");
+            System.out.printf("%n");
 
-        for(int i = 0; i < players.size(); i++){
-            for(int j = 0; j < players.get(i).getHandSize(); j++){
-                System.out.printf("|%3s  |  ", players.get(i).getCard(j).getSuit());
+            for(int i = s; i < playersperline; i++){
+                for(int j = 0; j < players.get(i).getHandSize(); j++){
+                    System.out.printf("|%3s  |  ", players.get(i).getCard(j).getSuit());
+                }
             }
-        }
-        System.out.printf("%n");
+            System.out.printf("%n");
 
-
-        for(int i = 0; i < players.size(); i++){
-            for(int j = 0; j < players.get(i).getHandSize(); j++){
-                System.out.printf("|%5s|  ", players.get(i).getCard(j).getRank());
+            for(int i = s; i < playersperline; i++){
+                for(int j = 0; j < players.get(i).getHandSize(); j++){
+                    System.out.printf("|%5s|  ", players.get(i).getCard(j).getRank());
+                }
             }
-        }
-        System.out.printf("%n");
+            System.out.printf("%n");
  
-        for(int i = 0; i < players.size(); i++){
-            for(int j = 0; j < players.get(i).getHandSize(); j++){
-                System.out.printf("%-9s", border);
+            for(int i = s; i < playersperline; i++){
+                for(int j = 0; j < players.get(i).getHandSize(); j++){
+                    System.out.printf("%-9s", border);
+                }
             }
-        }
-        System.out.printf("%n");
 
+            System.out.printf("%n%n");
+        }
     }
 }
